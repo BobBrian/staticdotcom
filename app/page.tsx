2614efc597109a9964baa0d13493299e320c89b1
BobBrian <rellrocker235@gmail.com>
2023-10-11 13:01:31 -0400

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+// import React from 'react'
+// //To get and display static data
+
+// const getPostData = async() => {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+//     return res.json();
+
+// }
+
+// export default async function AboutPage() {
+//  const posts = await getPostData()
+//  console.log(posts)
+
+//   return (
+//     <div>
+//        {posts.map((post: any) =>{
+//         return <p>{post.title}</p>
+//        })} 
+//     </div>
+//   )
+// }
+
 import React from 'react'
 
 export default function Home() {
