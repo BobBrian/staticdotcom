0b9da64382012a9f72f725f51a1c2d44f8391c8e
BobBrian <rellrocker235@gmail.com>
2023-10-12 15:33:38 -0400

    Added Fetch

--- app/useeffect/page.tsx
+++ app/useeffect/page.tsx
@@ -5,7 +5,7 @@ import {useState, useEffect} from 'react'
 
 export default function UseEffectFetch() {
 
-    const [todos,setTodo] = useState(null)
+    const [todos,setTodo] = useState([] as any[])
 
     //Code to fetch Data when ever it is rerendered
     useEffect(() =>{
@@ -17,10 +17,29 @@ export default function UseEffectFetch() {
         // console.log(data)
         setTodo(data)
        })
-        
+       .catch( err =>{
+        console.log(err.message)
+
+       })
     },[])
 
   return (
-    <div>UseEffectFetch</div>
+     <div>
+        {todos.map((todo)=>(
+            <div key={todo.id}>
+                <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg mb-6">
+
+                <h2 className="text-center">{todo.title}</h2>
+
+                <p className="text-center">Written by {todo.author}</p>
+
+                <button className="text-center inline-flex items-center px-3 py-2 text-sm font-medium 
+                text-center text-white bg-blue-700 rounded-lg" >Click Me</button>
+
+                </div>
+             
+            </div>
+        ))}
+    </div>
   )
 }
