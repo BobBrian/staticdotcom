5de1ed1fa47932abed79227871e0cb63ab3f33cb
BobBrian <rellrocker235@gmail.com>
2023-10-16 09:41:08 -0400

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+//This is the Final Verision of the Static code
+//The Purpose of this is to fill in the space for the Todo Application
+//This code is specfically for 4 functions
+//GET
+//GET with id
+//POST
+//DELETE
+import Link from 'next/link'
 import React from 'react'
 
-export default function Home() {
+
+export default async function Home() {
+  
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Welcome to the Static Todo List</h1>
+        <Link className="border border-slate-300 text-slate-300 px-2 py-1 
+        rounded hover:bg-slate-700 outline-none" href="/create">Create</Link>
+      </header>
+
+      
+    </>
+)}
