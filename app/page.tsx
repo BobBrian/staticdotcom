31cb666bde0cbf36ea43614a70f59ca1a59609e7
BobBrian <rellrocker235@gmail.com>
2023-10-23 12:41:08 -0400

    router

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+'use client'
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+
+  const router = useRouter()
+
+  
+
+  const handleSubmit = (e:any) => {
+      console.log("Redirecting to New Page")
+      router.push('/about')
+  }
+ 
   return (
-    <div>Home</div>
+    <form  >
+      <button type="button" onClick={handleSubmit}>
+       Click me
+    </button>
+
+    </form>
+    
   )
 }
